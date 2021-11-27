00795f11e7323d7c6c90372ca24d7756872e1e78
Exopl0x <jacobfactora@yahoo.com>
2021-11-26 23:16:18 -0800

    Finalized MusicManager and added Main Camera Prefab

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class MusicManager : MonoBehaviour
 {
     private AudioSource[] musics;
+    public AudioClip intro;
 
     void Awake()
     {
-        StartCoroutine(PlayMusic());
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
         musics = gameObject.GetComponents<AudioSource>();
+        StartCoroutine(PlayMusic());
     }
 
     IEnumerator PlayMusic()
     {
-        yield return new WaitForSecondsRealtime(3.34f);
+        // 1 second buffer time for the loop to start
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        // Halts Coroutine until the intro is done playing, then plays the loop.
+        yield return new WaitUntil(() => !musics[1].isPlaying);
         musics[0].Play();
+
         yield return null;
     }
 }
